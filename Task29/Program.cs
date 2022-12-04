5ec5b825561f8421143785f795990996c6fc9fd5
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-05 01:45:23 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задает массив из N элементов и выводит их на экран.
+//Вывод сделать отдельным методом.
+int number = ReadInt("Enter the number: ");
+int[] array = new int[number];
+for (int i=0; i< array.Length; i++)
+{
+    array[i] = new Random().Next(0,100);
+}
+
+Console.Write($"The random array is made up of {number} elements: ");
+Print1();
+
+
+void Print1()
+{
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
