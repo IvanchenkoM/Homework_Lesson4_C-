542ba4fb2a8ab4407f2f4e3cffbeb3c777aae10b
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-05 00:51:54 +0300

    Task25

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму чисел в числе.
+int number = ReadInt("Enter the number: ");
+int temp=number;
+int sum=0;
+int i=0;
+while (temp>0)
+{
+    sum +=temp%10;
+    temp/=10;
+    i++;
+}
+Console.Write($"The sum of the numbers in {number} is {sum}.");
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
