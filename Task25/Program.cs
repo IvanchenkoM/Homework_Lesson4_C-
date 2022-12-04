4cdf0c54add987c47860ee8a8fb2f0a0d0e9b318
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-05 00:29:40 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает два числа (А и В) и возводит число А в степень В.
+
+int numberFirst = ReadInt("Enter the first number: ");
+int numberSecond = ReadInt("Enter the second number: ");
+double result = 1;
+if (numberSecond>=0)
+{
+    for (int i=1; i<=numberSecond; i++)
+    {
+    result *= numberFirst; 
+    }
+}
+else
+{
+    for (int j=1; j<= -numberSecond; j++)
+    {
+    result /= numberFirst; 
+    }
+}
+
+Console.Write($"{numberFirst} to the {numberSecond}th power is {result}.");
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
